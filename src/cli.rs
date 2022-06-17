35cbf2c4251846291b0d98c3667fb274e4597fcf
Joost Molenaar <jjm@j0057.nl>
2022-06-17 23:08:29 +0200

    Add unit tests for clap parser

--- src/cli.rs
+++ src/cli.rs
@@ -19,3 +19,41 @@ impl CLI {
         return Self::try_parse();
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_long() -> Result<(), clap::Error> {
+        CLI::try_parse_from(["./foo", "--connect", "example.com:8000", "--listen", "0.0.0.0:9194"])?;
+        CLI::try_parse_from(["./bar", "--serial", "/dev/ttyS0", "--listen", "0.0.0.0:9194"])?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_short() -> Result<(), clap::Error> {
+        CLI::try_parse_from(["./foo", "-c", "example.com:4000", "-l", "0.0.0.0:9194"])?;
+        Ok(())
+    }
+
+    #[test]
+    fn test_source_missing() {
+        assert!(CLI::try_parse_from(["./foo", "-l", "0.0.0.0:9194"]).is_err());
+    }
+
+    #[test]
+    fn test_source_connect() {
+        assert!(CLI::try_parse_from(["./foo", "-l", "0.0.0.0:9194", "-c", "example.com:8000"]).is_ok());
+    }
+
+    #[test]
+    fn test_source_serial() {
+        assert!(CLI::try_parse_from(["./foo", "-l", "0.0.0.0:9194", "-s", "/dev/ttyS0"]).is_ok());
+    }
+
+    #[test]
+    fn test_source_1_too_many() {
+        assert!(CLI::try_parse_from(["./foo", "-l", "0.0.0.0:9194", "-c", "example.com:8000", "-s", "/dev/ttyS0"]).is_err());
+    }
+}
